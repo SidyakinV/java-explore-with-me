648bdb34870f2f66b84a88e848551c1b95ec6051
Vitaly <vseed@mail.ru>
2024-07-09 07:45:42 +0300

    style: unused import

--- main-service/src/main/java/ru/practicum/ewm/repositories/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repositories/CompilationRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.models.Compilation;
 
-import java.util.List;
-
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
 
     Slice<Compilation> findAllByPinned(Boolean pinned, Pageable pageable);
